7d91ea9ce71e9f607312e96b5555a3186354bd72
sokens <edisokenou@gmail.com>
2026-04-18 06:08:34 +0100

    Ajustement QR Code, Lieu et passage au masque blanc pur

--- src/certificates/certificates.service.ts
+++ src/certificates/certificates.service.ts
@@ -246,7 +246,7 @@ export class CertificatesService {
         
         page.drawImage(qrImage, {
             x: 85, // Encore décalé vers la droite
-            y: 85, // Légèrement redescendu pour s'éloigner de l'icône calendrier
+            y: 88, // Légèrement remonté
             width: 75,
             height: 75
         });
@@ -255,7 +255,7 @@ export class CertificatesService {
         const refWidth = fontBold.widthOfTextAtSize(ticketRef, 10);
         page.drawText(ticketRef, {
             x: 85 + (75 - refWidth) / 2, // Centré sous le QR
-            y: 70, // Redescendu également
+            y: 73, // Légèrement remonté
             size: 10,
             font: fontBold,
             color: rgb(0.2, 0.08, 0.05)
